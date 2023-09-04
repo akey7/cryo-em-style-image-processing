a790713e49ee66f15e9ab6a4920378d4fbff4aad
Alicia Key <alicia@akey7.com>
2023-09-04 11:48:39 -0600

    Update transform_patterns.py

--- transform_patterns.py
+++ transform_patterns.py
@@ -10,10 +10,20 @@ def fourier_transform_2D(data):
     # Shift the zero-frequency component to the center
     f_shift = np.fft.fftshift(f_transform)
 
+    # Compute power spectrum
+    power_spectrum = np.power(f_shift, 2)
+
     # Compute magnitude spectrum
     magnitude_spectrum = np.abs(f_shift)
 
     return magnitude_spectrum
+
+
+def process_image(input_image_path, output_image_path):
+    img = Image.open(input_image_path).convert("L")
+    power_spectrum = fourier_transform_2D(img)
+    power_spectrum_img = Image.fromarray(power_spectrum, "L")
+    power_spectrum_img.save(output_image_path)
 
 
 def main():
@@ -22,8 +32,11 @@ def main():
     for root, _, files in os.walk(directory_path):
         for f in files:
             if f.endswith(".png"):
-                file_path = os.path.join(root, f)
-                print("File:", file_path)
+                input_image_path = os.path.join(root, f)
+                base = f.replace(".png", "")
+                output_image_path = os.path.join("output", f'{base}_ft.png')
+                process_image(input_image_path, output_image_path)
+                print("done!", input_image_path, output_image_path)
 
 
 if __name__ == "__main__":
