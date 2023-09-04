a0ccd8add9863de4c8280d91288c672104a665f8
Alicia Key <alicia@akey7.com>
2023-09-04 12:05:53 -0600

    Update paint_patterns.py

--- paint_patterns.py
+++ paint_patterns.py
@@ -1,6 +1,7 @@
-from os.path import join
+import os
 import numpy as np
 from PIL import Image
+import math
 
 
 def one_rect(rect_width=50, rect_height=50, image_width=100, image_height=100):
@@ -15,10 +16,24 @@ def one_rect(rect_width=50, rect_height=50, image_width=100, image_height=100):
     return grayscale
 
 
+def sines(h=1, k=1, image_width=100, image_height=100):
+    grayscale = np.zeros((image_height, image_width)).astype(np.uint8)
+
+    for x in range(image_width):
+        for y in range(image_height):
+            value = (math.sin(2 * h * math.pi * x / image_width) / 2) + 0.5
+            grayscale[y, x] = math.floor(value * 255)
+
+    return grayscale
+
+
 def main():
-    grayscale = one_rect()
-    img = Image.fromarray(grayscale, "L")
-    img.save(join("input", "one_rect.png"))
+    grayscale_01 = one_rect()
+    img_01 = Image.fromarray(grayscale_01, "L")
+    img_01.save(os.path.join("input", "one_rect.png"))
+    grayscale_02 = sines()
+    img_02 = Image.fromarray(grayscale_02, "L")
+    img_02.save(os.path.join("input", "sines_h=1_k=1.png"))
 
 
 if __name__ == "__main__":
