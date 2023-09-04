c17c63afcb61c65e1b81ebd1eb3197c06b95afd0
Alicia Key <alicia@akey7.com>
2023-09-04 09:07:04 -0600

    Update paint_patterns.py

--- paint_patterns.py
+++ paint_patterns.py
@@ -1,18 +1,26 @@
 from os.path import join
 import numpy as np
 from PIL import Image
+from math import floor
 
-def save_rgb_as_png(red, green, blue, filename):
-    """
-    Saves three numpy arrays as red, green, and blue channels in a PNG file.
 
-    Parameters:
-    - red, green, blue: numpy arrays of the same shape representing the RGB channels.
-    - filename: the name of the PNG file to save.
-    """
-    # Stack arrays along the third dimension
-    image_array = np.stack((red, green, blue), axis=-1)
+def one_rect(rect_width=50, rect_height=50, image_width=100, image_height=100):
+    upper_left = ((image_width - rect_width) // 2, (image_height - rect_height) // 2)
+    lower_right = (upper_left[0] + rect_width, upper_left[1] + rect_height)    
+    grayscale = np.zeros((image_height, image_width)).astype(np.uint8)
     
-    # Convert to PIL Image and save
-    img = Image.fromarray(image_array.astype('uint8'))
-    img.save(filename)
+    for x in range(upper_left[0], lower_right[0]):
+        for y in range(upper_left[1], lower_right[1]):
+            grayscale[y, x] = 255
+
+    return grayscale
+
+
+def main():
+    grayscale = one_rect()
+    img = Image.fromarray(grayscale, 'L')
+    img.save(join('output', 'one_rect.png'))
+
+
+if __name__ == '__main__':
+    main()
